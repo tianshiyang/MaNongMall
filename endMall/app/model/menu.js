978aed2200ac7124999948c7e6b6f4677a034cc8
田世洋 <tianshiyang@icourt.cc>
2023-12-07 23:11:40 +0800

    feat: 菜单表

---
+++ endMall/app/model/menu.js
@@ -0,0 +1,46 @@
+"use strict"
+module.exports = (app) => {
+  const { STRING, BIGINT, BOOLEAN } = app.Sequelize
+
+  const Menu = app.model.define(
+    "menu",
+    {
+      id: {
+        type: BIGINT(11),
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+        comment: "主键ID",
+      },
+      menu_name: {
+        type: STRING(10),
+        allowNull: false,
+        comment: "菜单名称",
+      },
+      menu_path: {
+        type: STRING(30),
+        allowNull: false,
+        comment: "菜单路径",
+        unique: true,
+      },
+      menu_parent: {
+        type: BIGINT(11),
+        allowNull: false,
+        comment: "父级菜单ID",
+      },
+      is_delete: {
+        type: BOOLEAN,
+        defaultValue: false,
+        comment: "逻辑删除",
+      },
+    },
+    {
+      freezeTableName: true, // 取消复数
+      timestamps: true, // 开启时间戳
+      createdAt: "create_time",
+      updatedAt: "update_time",
+    }
+  )
+
+  return Menu
+}
