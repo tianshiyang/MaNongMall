c6b6da4fe045f82da91e71f3648c1fe9d59b35f7
田世洋 <tianshiyang@icourt.cc>
2023-12-09 19:06:24 +0800

    feat: 创建、编辑权限

--- endMall/app/controller/permission/index.js
+++ endMall/app/controller/permission/index.js
@@ -4,21 +4,60 @@ class PermissionController extends BaseController {
   // 编辑、创建权限
   async updatePermission() {
     // Post请求，通过this.ctx.request.body获取参数
+    // 获取参数
+    const params = this.ctx.request.body
     // 定义校验规则
     const rules = {
       permission_name: "string",
-      permission_sign: "string",
       permission_remark: "string",
     }
+    if (!params.permission_id) {
+      // 创建 -> 需要校验权限标识
+      rules.permission_sign = "string"
+    } else {
+      // 编辑 -> 需要校验权限唯一ID
+      rules.permission_id = "int"
+    }
     // 校验参数
-    const errors = await this.app.validator.validate(
-      rules,
-      this.ctx.request.body
-    )
+    const errors = await this.app.validator.validate(rules, params)
     if (errors) {
       // 如果Errors有值,则代表参数校验失败，调用自定义的error返回结果
       this.error({ error_message: `${errors[0].field}: ${errors[0].message}` })
       return
+    }
+
+    if (params.permission_id) {
+      // 编辑
+      let result = null
+      try {
+        result = await this.ctx.service.permission.index.updatePermission(
+          params
+        )
+      } catch (e) {
+        return this.error({ error_message: e.errors[0].message })
+      }
+      if (result[0] === 0) {
+        // 编辑的时候，可能因为ID不存在，所以没有数据被更改
+        return this.error({ error_message: "没有此条权限" })
+      }
+      this.success({
+        message: "操作成功！",
+      })
+    } else {
+      // 新增
+      let result = null
+      try {
+        result = await this.ctx.service.permission.index.createPermission(
+          params
+        )
+      } catch (e) {
+        return this.error({ error_message: e.errors[0].message })
+      }
+      // 返回创建完成的权限信息
+      this.success({
+        ...result.dataValues,
+        message: "创建成功！",
+      })
     }
   }
 }
--- endMall/app/service/permission/index.js
+++ endMall/app/service/permission/index.js
@@ -16,6 +16,24 @@ class PermissionService extends Service {
       permission_remark,
     })
   }
+  /* 编辑权限
+   * @param {Object} - {permission_id, permission_name, permission_remark} 权限唯一id 权限名称 权限描述
+   * @returns {Object} 创建信息
+   */
+  async updatePermission({
+    permission_id,
+    permission_name,
+    permission_remark,
+  }) {
+    return await this.ctx.model.Permission.update({
+      permission_name,
+      permission_remark,
+    }, {
+      where: {
+        id: permission_id,
+      },
+    })
+  }
 }
 
 module.exports = PermissionService
