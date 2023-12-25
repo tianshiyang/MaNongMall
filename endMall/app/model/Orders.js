065a2fea74a0fd5b4b76d11e8c2564807256851b
田世洋 <tianshiyang@icourt.cc>
2023-12-25 23:43:30 +0800

    feat: 订单表

---
+++ endMall/app/model/Orders.js
@@ -0,0 +1,45 @@
+"use strict"
+module.exports = (app) => {
+  const { BIGINT, DECIMAL, INTEGER } = app.Sequelize
+
+  const Orders = app.model.define(
+    "orders",
+    {
+      id: {
+        type: BIGINT(11),
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+        comment: "主键ID",
+      },
+      goods_id: {
+        type: BIGINT(11),
+        allowNull: false,
+        comment: "商品id",
+      },
+      seller_id: {
+        type: BIGINT(11),
+        allowNull: false,
+        comment: "销售id",
+      },
+      sales_num: {
+        type: INTEGER,
+        allowNull: false,
+        comment: "售卖数量",
+      },
+      profit: {
+        type: DECIMAL(11, 2),
+        allowNull: false,
+        comment: "获利",
+      },
+    },
+    {
+      freezeTableName: true, // 取消复数
+      timestamps: true, // 开启时间戳
+      createdAt: "create_time",
+      updatedAt: "update_time",
+    }
+  )
+
+  return Orders
+}
