9506a43493ca582f321000ac6a27d36f360f526f
田世洋 <tianshiyang@icourt.cc>
2023-12-26 11:11:30 +0800

    feat: 角色商品分类表

---
+++ endMall/app/model/RoleGoodsClassification.js
@@ -0,0 +1,35 @@
+"use strict"
+module.exports = (app) => {
+  const { BIGINT } = app.Sequelize
+
+  const RoleGoodsClassification = app.model.define(
+    "role_goods_classification",
+    {
+      id: {
+        type: BIGINT(11),
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+        comment: "主键ID",
+      },
+      goods_classification_id: {
+        type: BIGINT(11),
+        allowNull: false,
+        comment: "商品分类表主键ID",
+      },
+      role_id: {
+        type: BIGINT(11),
+        allowNull: false,
+        comment: "角色表主键id",
+      },
+    },
+    {
+      freezeTableName: true, // 取消复数
+      timestamps: true, // 开启时间戳
+      createdAt: "create_time",
+      updatedAt: "update_time",
+    }
+  )
+
+  return RoleGoodsClassification
+}
